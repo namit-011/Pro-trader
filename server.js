44ad580061e12ee4546a446503e8dd8d743f999f
ProTrader <protrader@deploy.com>
2026-03-11 16:37:04 +0530

    Fix: Express 5 catch-all route + upgrade to Node 22

--- server.js
+++ server.js
@@ -867,7 +867,7 @@ app.get('/api/country/:iso', async (req, res) => {
 // Serve React build in production
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'dist')));
-    app.get('*', (req, res) => {
+    app.use((req, res) => {
         res.sendFile(path.join(__dirname, 'dist', 'index.html'));
     });
 }
